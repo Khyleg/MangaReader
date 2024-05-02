e3ea0e336d894519355cc323db22c8eb102441c9
Khyleg <gainkhyle@gmail.com>
2024-05-02 23:13:09 +0800

    Now displaying random mangas per refresh and added overflow hidden for the summary

--- src/components/MangaDexHelper.jsx
+++ src/components/MangaDexHelper.jsx
@@ -5,13 +5,18 @@ export const fetchRandomManga = () => {
             const en_title = data.data.attributes.title.en;
             const cover_id = data.data.relationships[2].id;
             const summary = data.data.attributes.description.en;
+            const manga_id = data.data.id;
             return fetchFileName(cover_id)
                 .then(fileName => {
+                    const cover_url = fetchCoverUrl(manga_id, fileName);
+                    console.log("Fetching Cover of Manga: " + en_title + " with cover link: " + cover_url);
                     const MangaInformation = {
                         "en_title": en_title,
                         "cover_id": cover_id,
                         "summary" : summary,
-                        "fileName": fileName
+                        "fileName": fileName,
+                        "manga_id" : manga_id,
+                        "cover_url" : cover_url
                     };
                     return MangaInformation;
                 });
@@ -25,9 +30,14 @@ export const fetchFileName = (cover_id) => {
     return fetch("https://api.mangadex.org/cover/" + cover_id)
         .then(response => response.json())
         .then(data => {
+            console.log(data);
             return data.data.attributes.fileName;
         })
         .catch(error => {
             console.error("Error fetching file name:", error);
         });
+}
+
+export const fetchCoverUrl = (manga_id, file_name) => {
+    return "https://uploads.mangadex.org/covers/" + manga_id + "/" + file_name;
 }
--- src/components/RandomMangaContainer.jsx
+++ src/components/RandomMangaContainer.jsx
@@ -4,16 +4,21 @@ import "./RandomMangaContainer.css";
 export default function RandomMangaContainer() {
     const [mangaInformation, GetInformation] = useState();
     useEffect(() => {
-        fetchRandomManga().then(mangaInformation => {
+        fetchRandomManga()
+        .then(mangaInformation => {
             GetInformation(mangaInformation);
         });
     },[]);
+    console.log(mangaInformation);
     if(mangaInformation) {
         return (
             <div className="container">
-                <h3>{mangaInformation["en_title"]}</h3>
-                <h3>{mangaInformation["summary"]}</h3>
-
+                <div className="description-text">
+                    <h2>{mangaInformation["en_title"]}</h2>
+                    <br></br><br></br><br></br>
+                    <h3>{mangaInformation["summary"]}</h3>
+                </div>
+                <img src={mangaInformation["cover_url"]} width={150} height={270} />
             </div>
         )
     }
