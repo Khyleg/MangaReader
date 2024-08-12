d5d1c1258e56cbc9e4ee9eb0cde8c2a61cdc5fe4
Khyleg <gainkhyle@gmail.com>
2024-08-13 01:09:40 +0800

    unneeeded commit lol

--- src/components/ViewManga.jsx
+++ src/components/ViewManga.jsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function ViewManga() {
     console.log("yikes");
+    const params = useParams();
+    console.log(params.mangaurl);
     const [manga, setManga] = useState([]);
+    console.log(`Fetching: http://localhost:3000/scrapemanga?path=manga/Boku-No-Hero-Academia`);
     useEffect(() => {
-        fetch('http://localhost:3000/scrapehot')
+        fetch(`http://localhost:3000/scrapemanga?path=manga/Boku-No-Hero-Academia`)
           .then(response => response.json())
           .then(data => setManga(data))
           .catch(error => console.error('Error fetching data:', error));
       }, []);
     return(
-        <p>Test</p>
+        <p>{manga}</p>
     );
 }
 export default ViewManga;
